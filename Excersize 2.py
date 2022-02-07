8cea485dee7ef90d8801d5ee6261218db607050c
joelrichy <richardson@middleton.school.nz>
2022-02-08 12:03:24 +1300

    hello
    hell

---
+++ Excersize 2.py
@@ -0,0 +1,6 @@
+numbers = [20,21,22,23,24,25]
+for x in range(len(numbers)):
+    print(numbers[x])
+sum = sum(numbers)
+print(sum)
+
---
+++ Excersizes.py
@@ -0,0 +1,7 @@
+numberOne = int(input("Input 1st number: "))
+numberTwo = int(input("Input number 2: "))
+
+if numberOne > numberTwo:
+    print(numberOne)
+else:
+    print(numberTwo)
